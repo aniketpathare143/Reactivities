84d38ee3b51b45dabd5380b882526bbad07e5f7b
Aniket Pathare <aniket.pathare@rotary.org>
2023-10-09 20:55:40 +0530

    End of section 11

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -2,6 +2,7 @@ import { makeAutoObservable, runInAction } from "mobx";
 import { Activity } from "../models/activity";
 import agent from "../api/agent";
 import { v4 as uuid } from 'uuid'
+import { format } from "date-fns";
 
 export default class ActivityStore {
 
@@ -19,13 +20,14 @@ export default class ActivityStore {
 
     //ComputedProperties
     get activitiesByDate() {
-        return Array.from(this.activityRegistry.values()).sort((a, b) => Date.parse(a.date) - Date.parse(b.date));
+        return Array.from(this.activityRegistry.values()).sort((a, b) =>
+            a.date!.getTime() - b.date!.getTime());
     }
 
     get groupedActivities() {
         return Object.entries(
             this.activitiesByDate.reduce((activities, activity) => {
-                const date = activity.date;
+                const date = format(activity.date!, 'dd MMM yyyy')
                 activities[date] = activities[date] ? [...activities[date], activity] : [activity];
                 return activities;
             }, {} as { [key: string]: Activity[] })
@@ -73,7 +75,7 @@ export default class ActivityStore {
     }
 
     private setActivity = (activity: Activity) => {
-        activity.date = activity.date.split('T')[0];
+        activity.date = new Date(activity.date!);
         this.activityRegistry.set(activity.id, activity);
     }
 
@@ -92,9 +94,9 @@ export default class ActivityStore {
             await agent.Activities.create(activity);
             runInAction(() => {
                 this.activityRegistry.set(activity.id, activity);
-                this.selectedActivity = activity;
-                this.loading = false;
+                this.selectedActivity = activity;                
                 this.editMode = false;
+                this.loading = false;
             })
         } catch (error) {
             console.log(error);
@@ -111,8 +113,8 @@ export default class ActivityStore {
             runInAction(() => {
                 this.activityRegistry.set(activity.id, activity);
                 this.selectedActivity = activity;
-                this.loading = false;
                 this.editMode = false;
+                this.loading = false;
             })
         } catch (error) {
             console.log(error);
